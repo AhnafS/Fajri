324044520df183ccc21db221c4889d65845db26f
AhnafHamim <hamimnycusa@gmail.com>
2023-10-13 23:57:59 -0400

    Added pics

--- consts.js
+++ consts.js
@@ -1,75 +1,98 @@
 const allProducts = [
   {
     id: 0,
-    name: "Bespoke Emerald",
+    name: "Bespoke Midnight",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
-    image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+    desc: "Introducing Fajri's Bespoke Royal Emerald Thobe. \nElevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    image:"assets/KV_09423copy.webp",
   },
   {
     id: 1,
     name: "Bespoke Emerald",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing Fajri's Bespoke Royal Midnight Thobe. \nElevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_09323.webp",
   },
   {
     id: 2,
-    name: "Bespoke Emerald",
+    name: "Bespoke Ocean",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing Fajri's Bespoke Royal Ocean Thobe. \nElevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_09429.webp",
   },
   {
     id: 3,
-    name: "Bespoke Emerald",
+    name: "Bespoke Sahara",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing Fajri's Bespoke Royal Sahara Thobe. \nElevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_09398.webp",
   },
   {
     id: 4,
-    name: "Bespoke Emerald",
+    name: "Cloud",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing 'The Cloud' Hooded Thobe - Your Ultimate Autumn Wardrobe Essential. \nMade from high-quality fabric, 'The Cloud' Hooded Thobe offers supreme comfort that's perfect for autumn. It's not too heavy or too light - it strikes the ideal balance to keep you cosy on cooler days without feeling overly warm. Don't miss your chance to own 'The Cloud' Hooded Thobe, the perfect addition to your autumn wardrobe. Embrace the changing seasons with grace and style. Order yours today and experience the luxury of Moroccan fashion.",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_08907.webp",
   },
   {
     id: 5,
-    name: "Bespoke Emerald",
+    name: "Deep Ocean",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing 'The Deep Ocean' Hooded Thobe - Your Ultimate Autumn Wardrobe Essential. \nMade from high-quality fabric, 'The Deep Ocean' Hooded Thobe offers supreme comfort that's perfect for autumn. It's not too heavy or too light - it strikes the ideal balance to keep you cosy on cooler days without feeling overly warm. Don't miss your chance to own 'The Deep Ocean' Hooded Thobe, the perfect addition to your autumn wardrobe. Embrace the changing seasons with grace and style. Order yours today and experience the luxury of Moroccan fashion.",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_09030.webp",
   },
   {
     id: 6,
-    name: "Bespoke Emerald",
+    name: "Eclipse",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing 'Eclipse' Hooded Thobe - Your Ultimate Autumn Wardrobe Essential. \nMade from high-quality fabric, 'The Deep Ocean' Hooded Thobe offers supreme comfort that's perfect for autumn. It's not too heavy or too light - it strikes the ideal balance to keep you cosy on cooler days without feeling overly warm. Don't miss your chance to own 'The Deep Ocean' Hooded Thobe, the perfect addition to your autumn wardrobe. Embrace the changing seasons with grace and style. Order yours today and experience the luxury of Moroccan fashion.",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_09080.webp",
   },
   {
     id: 7,
-    name: "Bespoke Emerald",
+    name: "Jungle",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "Introducing 'Jungle' Hooded Thobe - Your Ultimate Autumn Wardrobe Essential. \nMade from high-quality fabric, 'The Deep Ocean' Hooded Thobe offers supreme comfort that's perfect for autumn. It's not too heavy or too light - it strikes the ideal balance to keep you cosy on cooler days without feeling overly warm. Don't miss your chance to own 'The Deep Ocean' Hooded Thobe, the perfect addition to your autumn wardrobe. Embrace the changing seasons with grace and style. Order yours today and experience the luxury of Moroccan fashion.",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/KV_09202.webp",
   },
   {
     id: 8,
-    name: "Bespoke Emerald",
+    name: "Royal Gold Under Piece",
     price: 88.0,
-    desc: "Elevate your style to regal heights with our latest masterpiece. Crafted with the utmost precision and adorned with intricate hand embroidery, this collection represents the pinnacle of Moroccan craftsmanship and sophistication. Each piece in this collection is a work of art, meticulously handcrafted by skilled artisans who have honed their craft for generations. The result? A thobe that exudes an aura of opulence, fit for royalty.  It's time to make a statement that truly stands the test of time. ",
+    desc: "An Exclusive Fajri piece. Featuring a Black Zipped Under Piece made from the finest quality paired with Gold intricate embroidery. A true fashion statement, made for Royalty. \nThe Royal Gold can be worn on its own, or paired with any of our Royal Over Pieces.",
     image:
-      "https://maghribi.co/cdn/shop/files/KV_09323.jpg?v=1695566213&width=2200",
+      "assets/OMJ6151.webp",
+  },
+  {
+    id: 9,
+    name: "Royal Gold Over Piece",
+    price: 88.0,
+    desc: "An Exclusive Fajri piece. Featuring a White see-through Over Piece made from the finest quality paired with intricate embroidery. A true fashion statement, made for Royalty. ",
+    image:
+      "assets/image.webp",
+  },
+  {
+    id: 10,
+    name: "Royal Ocean Under Piece",
+    price: 88.0,
+    desc: "An Exclusive Maghribi piece. Featuring a White Zipped Under Piece made from the finest quality paired with Navy Blue intricate embroidery. A true fashion statement, made for Royalty. \nThe Royal Ocean Under Piece can be worn on its own, or paired with the Royal Ocean Over Piece. ",
+    image:
+      "assets/OMJ5985.webp",
+  },
+  {
+    id: 10,
+    name: "Royal Dusk",
+    price: 88.0,
+    desc: "An Exclusive Maghribi piece. Featuring a Burgundy see-through Over Piece made from the finest quality paired with intricate embroidery. A true fashion statement, made for Royalty. \nPlease Note: The Under Piece must be purchased separately here. This listing is only for the Royal Dusk over thobe.",
+    image:
+      "assets/OMJ5906.webp",
   },
 ];
 
