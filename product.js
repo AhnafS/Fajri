ec895e015ef1c70f3f04053de17d98c3a2a10985
AhnafHamim <hamimnycusa@gmail.com>
2023-10-14 23:08:15 -0400

    Added Customer reviews and Info

--- product.js
+++ product.js
@@ -47,19 +47,19 @@ const testimonials = [
     position: "Marketing",
     photo:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6",
-    text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+    text: "I absolutely love the thobes I purchased from this company. The material is lightweight and breathable, perfect for the hot climate here. The embroidery and details are exquisite. I always get compliments when I wear these thobes.",
   },
   {
     name: "June Cha",
     position: "Software Engineer",
     photo: "https://randomuser.me/api/portraits/women/44.jpg",
-    text: "This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!",
+    text: "I struggled to find thobes that properly fit my tall, slim frame until I found this company. Their custom tailoring options ensure a perfect fit. The thobes drape beautifully and are ideal for both casual and formal wear.",
   },
   {
     name: "Iida Niskanen",
     position: "Data Entry",
     photo: "https://randomuser.me/api/portraits/women/68.jpg",
-    text: "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+    text: "I appreciate that this thobe company uses natural, breathable fabrics that are ideal for our hot climate. The thobes are lightweight yet durable. I receive many compliments whenever I wear them.",
   },
   {
     name: "Renee Sims",
@@ -71,20 +71,20 @@ const testimonials = [
     name: "Jonathan Nunfiez",
     position: "Graphic Designer",
     photo: "https://randomuser.me/api/portraits/men/43.jpg",
-    text: "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
+    text: "As a businessman, I appreciate the elegant and professional look of the thobes from this company. They are perfect for meetings and formal events, allowing me to stay cool and comfortable even in warm offices.",
   },
   {
     name: "Sasha Ho",
     position: "Accountant",
     photo:
       "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb",
-    text: "This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!",
+    text: "The high level of customer service from this thobe company is unmatched. They provided custom alterations for my unusual measurements at no extra cost, ensuring the perfect tailored fit.",
   },
   {
     name: "Veeti Seppanen",
     position: "Director",
     photo: "https://randomuser.me/api/portraits/men/97.jpg",
-    text: "This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.",
+    text: "I struggle to find modest yet stylish thobes for my tall and broad-shouldered frame. This company offers a wide selection of sizes and styles that actually fit me properly.",
   },
 ];
 
