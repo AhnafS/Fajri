c4dfe431f50c2a3cff187f6199edafeefe9deafc
AhnafHamim <hamimnycusa@gmail.com>
2023-10-14 23:27:02 -0400

    Added Customer Pictures

--- product.js
+++ product.js
@@ -43,48 +43,42 @@ const role = document.querySelector(".role");
 
 const testimonials = [
   {
-    name: "Miyah Myles",
-    position: "Marketing",
+    name: "Tameem Ahmed",
+    position: "Software Engineer",
     photo:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6",
+      "assets/1688489015292 (1).jpg",
     text: "I absolutely love the thobes I purchased from this company. The material is lightweight and breathable, perfect for the hot climate here. The embroidery and details are exquisite. I always get compliments when I wear these thobes.",
   },
   {
-    name: "June Cha",
-    position: "Software Engineer",
-    photo: "https://randomuser.me/api/portraits/women/44.jpg",
+    name: "Muhammad Munir",
+    position: "Data Scientist",
+    photo: "assets/1689558783383.jpg",
     text: "I struggled to find thobes that properly fit my tall, slim frame until I found this company. Their custom tailoring options ensure a perfect fit. The thobes drape beautifully and are ideal for both casual and formal wear.",
   },
   {
-    name: "Iida Niskanen",
+    name: "Arib Mahboob",
     position: "Data Entry",
-    photo: "https://randomuser.me/api/portraits/women/68.jpg",
+    photo: "assets/1689558783383.jpg",
     text: "I appreciate that this thobe company uses natural, breathable fabrics that are ideal for our hot climate. The thobes are lightweight yet durable. I receive many compliments whenever I wear them.",
   },
   {
     name: "Renee Sims",
     position: "Receptionist",
     photo: "https://randomuser.me/api/portraits/women/65.jpg",
-    text: "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
+    text: "I wore one of these elegant thobes to my brother's wedding in Dubai last year. I received so many compliments from family who were impressed by the fine details and intricate embroidery.",
   },
   {
-    name: "Jonathan Nunfiez",
+    name: "Sazid Omi",
     position: "Graphic Designer",
-    photo: "https://randomuser.me/api/portraits/men/43.jpg",
+    photo: "assets/1690416242903",
     text: "As a businessman, I appreciate the elegant and professional look of the thobes from this company. They are perfect for meetings and formal events, allowing me to stay cool and comfortable even in warm offices.",
   },
   {
-    name: "Sasha Ho",
-    position: "Accountant",
+    name: "Ahnaf Shamim",
+    position: "Fullstack Engineer",
     photo:
-      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb",
+      "assets/1670559620434.jpg",
     text: "The high level of customer service from this thobe company is unmatched. They provided custom alterations for my unusual measurements at no extra cost, ensuring the perfect tailored fit.",
-  },
-  {
-    name: "Veeti Seppanen",
-    position: "Director",
-    photo: "https://randomuser.me/api/portraits/men/97.jpg",
-    text: "I struggle to find modest yet stylish thobes for my tall and broad-shouldered frame. This company offers a wide selection of sizes and styles that actually fit me properly.",
   },
 ];
 
